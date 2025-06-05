dcec5ec9e1793ba8aeb558d845207fc5ab422d7a
Ashwin Prabou <aprabou@ucsc.edu>
2025-06-05 16:50:12 -0700

    no more phone number option

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,7 +11,6 @@ import { ShoppingBagIcon } from "lucide-react";
 
 const Register: React.FC = () => {
   const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
@@ -22,7 +21,7 @@ const Register: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!email || !phone || !password || !confirmPassword) {
+    if (!email || !password || !confirmPassword) {
       setError("Please fill in all fields");
       return;
     }
@@ -40,7 +39,7 @@ const Register: React.FC = () => {
     try {
       setError("");
       setLoading(true);
-      const { error } = await signUp(email, password, phone);
+      const { error } = await signUp(email, password);
 
       if (error) throw error;
 
@@ -103,27 +102,6 @@ const Register: React.FC = () => {
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="phone"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Phone number
-              </label>
-              <div className="mt-1">
-                <input
-                  id="phone"
-                  name="phone"
-                  type="phone"
-                  autoComplete="phone"
-                  required
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
                 />
               </div>
